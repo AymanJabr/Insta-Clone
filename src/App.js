bf7c3bd7d4f367254f49af2902cc2ae34139ecd9
ayman <aymanjaber2012@gmail.com>
2021-06-05 22:19:17 +0300

    Add login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import('./pages/login'));
+const SignUp = lazy(() => import('./pages/sign-up'));
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Suspense fallback={<p>Loading....</p>}>
       <Switch>
         <Route path={ROUTES.LOGIN} component={Login} />
+        <Route path={ROUTES.SIGH_UP} component={SignUp} />
       </Switch>
     </Suspense>
   );
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,36 +1,33 @@
 import { useContext, useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
-import * as ROUTES from '../constants/routes';
+// import * as ROUTES from '../constants/routes';
 
 /* eslint-disable no-unused-vars */
 
-export default function Login() {
+export default function SignUp() {
   const history = useHistory();
   const { firebase } = useContext(FirebaseContext);
 
+  const [userName, setUserName] = useState('');
+  const [fullName, setFullName] = useState('');
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
 
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = async (event) => {
+  const handleSignUp = async (event) => {
     event.preventDefault();
 
-    try {
-      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+    // try {
 
-      history.push(ROUTES.DASHBOARD);
-    } catch (error) {
-      setEmailAddress('');
-      setPassword('');
-      setError(error.message);
-    }
+    // } catch (error) {
+    // }
   };
 
   useEffect(() => {
-    document.title = 'Login -> Insta-Clone';
+    document.title = 'Sign Up -> Insta-Clone';
   }, []);
 
   return (
@@ -47,13 +44,33 @@ export default function Login() {
           </h1>
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-          <form onSubmit={handleLogin} method="POST">
+          <form onSubmit={handleSignUp} method="POST">
+
+            <input
+              aria-label="Enter your username"
+              type="text"
+              placeholder="Username"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setUserName(target.value)}
+              value={userName || ''}
+            />
+
+            <input
+              aria-label="Enter your full name"
+              type="text"
+              placeholder="Full name"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setFullName(target.value)}
+              value={fullName || ''}
+            />
+
             <input
               aria-label="Enter your email address"
               type="text"
               placeholder="Email address."
               className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
               onChange={({ target }) => setEmailAddress(target.value)}
+              value={emailAddress || ''}
             />
 
             <input
@@ -62,14 +79,16 @@ export default function Login() {
               placeholder="Password."
               className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
               onChange={({ target }) => setPassword(target.value)}
+              value={password || ''}
             />
-            <button disabled={isInvalid} type="submit" className={`bg-blue-500  text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-40'}`}> Login </button>
+
+            <button disabled={isInvalid} type="submit" className={`bg-blue-500  text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-40'}`}> Sign Up </button>
           </form>
         </div>
 
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
           <p className="text-sm">
-            Don&apos;t have an account?
+            Have an account?
             <Link to="/signup" className="font-bold text-blue-medium">
               Sign up
             </Link>
